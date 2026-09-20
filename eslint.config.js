9ec38669ab25b8b9f40aa89042e255e28ab67990
unevil-warden-scallion-princess-no-rollback <unevil-warden-scallion-princess-no-rollback@unevil-warden-scallion-princess-no-rollbacks-MacBook-Air.local>
2026-09-20 11:56:24 -0400

    Ignore generated client bundle in lint

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 
 export default tseslint.config(
-  { ignores: ["dist", "node_modules", "coverage"] },
+  { ignores: ["dist", "client", "node_modules", "coverage"] },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
